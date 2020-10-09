bdfd4fdb18d61f1fa2425b0c2db9f264608aa2b5
Mark Dell <madelk@gmail.com>
2020-10-08 22:44:57 -0500

    Theme hospital and Elite Plus

--- data/0A49446E9535F060AC91FDEC36AE89EA2FA351675BEC511DAD2843F069C0158B/process.psm1
+++ data/0A49446E9535F060AC91FDEC36AE89EA2FA351675BEC511DAD2843F069C0158B/process.psm1
@@ -1,4 +1,6 @@
 ﻿function Format-Game {
     Param ([Parameter(Mandatory = $true, Position = 0)] [string] $directory)
     Move-Item $directory$((Get-ChildItem -Path $directory -Directory)[0].Name)"\*" $directory
+    ((Get-Content -path ${directory}"hospital\hospital.cfg" -Raw) -replace 'D:\\HOSP','E:\HOSP') | Set-Content -Path ${directory}"hospital\hospital.cfg"
+    ((Get-Content -path ${directory}"hospital\sound\mdi.ini" -Raw) -replace 'SBPRO2.MDI','SBLASTER.MDI') | Set-Content -Path ${directory}"hospital\sound\mdi.ini"
 }
