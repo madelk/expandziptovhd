e49c6608be27594480cb361fedd19269f0827e4a
Mark Dell <madelk@gmail.com>
2020-10-09 20:01:48 -0500

    Theme and first enc fixes

--- data/563B2FBA96F6EA8E553EB3823A38AE011D24321EABD21DFBA70D2B5120821B27/process.psm1
+++ data/563B2FBA96F6EA8E553EB3823A38AE011D24321EABD21DFBA70D2B5120821B27/process.psm1
@@ -1,4 +1,5 @@
 ﻿function Format-Game {
     Param ([Parameter(Mandatory = $true, Position = 0)] [string] $directory)
     Move-Item $directory$((Get-ChildItem -Path $directory -Directory)[0].Name)"\*" $directory
+    ((Get-Content -path ${directory}firstenc.cfg -Raw) -replace 'D:','E:') | Set-Content -Path ${directory}firstenc.cfg
 }
